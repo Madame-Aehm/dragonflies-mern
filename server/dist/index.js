daf0c1931c117fa2721d934e2aceda2415434fa8
Madame-Aehm <emilyrachelstickler@gmail.com>
2025-07-30 14:54:57 +0200

    main function wrapper

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -11,31 +20,35 @@ require("dotenv/config");
 const mongoose_1 = __importDefault(require("mongoose"));
 const jwt_1 = require("./middlewares/jwt");
 const cloudinary_1 = __importDefault(require("./config/cloudinary"));
-const app = (0, express_1.default)();
-const port = process.env.PORT || 5000;
-app.use(express_1.default.json());
-app.use(express_1.default.urlencoded({
-    extended: true,
-}));
-app.use((0, cors_1.default)());
-(0, cloudinary_1.default)();
-app.use("/", express_1.default.static("documentation"));
-app.use("/images", express_1.default.static("uploads"));
-app.use(jwt_1.testingMiddleware);
-app.post('/testing', (req, res) => {
-    res.send('Hello World!');
+const main = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        if (!process.env.MONGO_URI) {
+            throw new Error("no mongo uri");
+        }
+        yield mongoose_1.default.connect(process.env.MONGO_URI);
+        const app = (0, express_1.default)();
+        app.use(express_1.default.json());
+        app.use(express_1.default.urlencoded({
+            extended: true,
+        }));
+        app.use((0, cors_1.default)());
+        (0, cloudinary_1.default)();
+        app.use("/", express_1.default.static("documentation"));
+        app.use("/images", express_1.default.static("uploads"));
+        app.use(jwt_1.testingMiddleware);
+        app.post('/testing', (req, res) => {
+            res.send('Hello World!');
+        });
+        app.use("/api/users", users_1.default);
+        app.use("/api/pets", pets_1.default);
+        app.use('/*splat', (req, res) => res.status(404).json({ error: "Endpoint not found." }));
+        const port = process.env.PORT || 5000;
+        app.listen(port, () => {
+            console.log("Connection to MongoDB established, and server is running on http://localhost:" + port);
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
 });
-app.use("/api/users", users_1.default);
-app.use("/api/pets", pets_1.default);
-app.use('/*splat', (req, res) => res.status(404).json({ error: "Endpoint not found." }));
-if (!process.env.MONGO_URI) {
-    throw new Error("no mongo uri");
-}
-mongoose_1.default
-    .connect(process.env.MONGO_URI)
-    .then(() => {
-    app.listen(port, () => {
-        console.log("Connection to MongoDB established, and server is running on http://localhost:" + port);
-    });
-})
-    .catch((err) => console.log(err));
+main();
--- server/index.ts
+++ server/index.ts
@@ -7,41 +7,47 @@ import mongoose from "mongoose";
 import { testingMiddleware } from "./middlewares/jwt";
 import configureCloudinary from "./config/cloudinary";
 
-const app = express();
-
-const port = process.env.PORT || 5000;
-
-app.use(express.json());
-app.use(
-    express.urlencoded({
-        extended: true,
-    })
-);
-app.use(cors());
-configureCloudinary();
-app.use("/", express.static("documentation"));
-app.use("/images", express.static("uploads"));
-app.use(testingMiddleware);
 
 
-app.post('/testing', (req, res) => {
-    res.send('Hello World!')
-});
+const main = async() => {
+    try {
+        if (!process.env.MONGO_URI) {
+            throw new Error("no mongo uri")
+        }
 
-app.use("/api/users", userRouter);
-app.use("/api/pets", petRouter);
+        await mongoose.connect(process.env.MONGO_URI)
 
-app.use('/*splat', (req, res) => res.status(404).json({ error: "Endpoint not found." }));
+        const app = express();
 
-if (!process.env.MONGO_URI) {
-    throw new Error("no mongo uri")
+        app.use(express.json());
+        app.use(
+            express.urlencoded({
+                extended: true,
+            })
+        );
+        app.use(cors());
+        configureCloudinary();
+        app.use("/", express.static("documentation"));
+        app.use("/images", express.static("uploads"));
+        app.use(testingMiddleware);
+
+
+        app.post('/testing', (req, res) => {
+            res.send('Hello World!')
+        });
+
+        app.use("/api/users", userRouter);
+        app.use("/api/pets", petRouter);
+
+        app.use('/*splat', (req, res) => res.status(404).json({ error: "Endpoint not found." }));
+
+        const port = process.env.PORT || 5000;
+        app.listen(port, () => {
+            console.log("Connection to MongoDB established, and server is running on http://localhost:" + port);
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => {
-        app.listen(port, () => {
-        console.log("Connection to MongoDB established, and server is running on http://localhost:" + port);
-        });
-    })
-    .catch((err) => console.log(err));
+main();
