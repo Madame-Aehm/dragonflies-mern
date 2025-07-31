32787654bb86e54c029c7b80551f27c5646c6296
Madame-Aehm <emilyrachelstickler@gmail.com>
2025-07-31 16:03:57 +0200

    return to pre-deploy state

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,39 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA, type VitePWAOptions } from 'vite-plugin-pwa'
-
-const manifestForPlugIn: Partial<VitePWAOptions> = {
-  registerType: "autoUpdate",
-  manifest:{
-    name:"Dragonflies MERN App!",
-    short_name:"GDF MERN",
-    description:"This app was created during live demos of MERN stack technologies.",
-    icons:[
-    {
-      src: 'assets/maskable_icon_x512.png',
-      sizes:'512x512',
-      type:'image/png',
-      purpose:'any maskable'
-    },
-    {
-      src:'assets/maskable_icon_x192.png',
-      sizes:'192x192',
-      type:'image/png',
-      purpose:'any maskable'
-    }
-  ],
-  theme_color:'#15ff00ff',
-  background_color:'#000000',
-  display:"standalone",
-  scope:'/',
-  start_url:"/",
-  orientation:'portrait'
-  }
-}
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA(manifestForPlugIn)],
+  plugins: [react()],
 })
--- server/index.ts
+++ server/index.ts
@@ -10,45 +10,42 @@ import path from "path";
 
 
 
-const main = async() => {
-    try {
-        if (!process.env.MONGO_URI) {
-            throw new Error("no mongo uri")
-        }
 
-        await mongoose.connect(process.env.MONGO_URI)
-
-        const app = express();
-
-        app.use(express.json());
-        app.use(
-            express.urlencoded({
-                extended: true,
-            })
-        );
-        app.use(cors());
-        configureCloudinary();
-        app.use("/", express.static(path.join(__dirname, "documentation")));
-        app.use("/images", express.static("uploads"));
-        app.use(testingMiddleware);
-
-
-        app.post('/testing', (req, res) => {
-            res.send('Hello World!')
-        });
-
-        app.use("/api/users", userRouter);
-        app.use("/api/pets", petRouter);
-
-        app.use('/*splat', (req, res) => res.status(404).json({ error: "Endpoint not found." }));
-
-        const port = process.env.PORT || 5000;
-        app.listen(port, () => {
-            console.log("Connection to MongoDB established, and server is running on http://localhost:" + port);
-        });
-    } catch (error) {
-        console.log(error);
-    }
+if (!process.env.MONGO_URI) {
+    throw new Error("no mongo uri")
 }
 
-main();
+const app = express();
+
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(cors());
+configureCloudinary();
+app.use("/", express.static(path.join(__dirname, "documentation")));
+app.use("/images", express.static("uploads"));
+app.use(testingMiddleware);
+
+
+app.post('/testing', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.use("/api/users", userRouter);
+app.use("/api/pets", petRouter);
+
+app.use('/*splat', (req, res) => res.status(404).json({ error: "Endpoint not found." }));
+
+const port = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(port, () => {
+        console.log("Connection to MongoDB established, and server is running on http://localhost:" + port);
+});
+    })
+    .catch((err) => {
+        console.log(err)
+    })
